069b68dcb3a08080dec144937e3ca5d28ede602e
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-07 23:43:58 +0530

    feat(authorizedguards): added authorized guards

--- src/app/core/auth/auth.routes.ts
+++ src/app/core/auth/auth.routes.ts
@@ -1,13 +1,16 @@
 import { Routes } from '@angular/router';
 import { AuthFormComponent } from './pages/auth-form/auth-form.component';
+import { UserUnauthorizedAuthGuard } from './pages/guards/user-unauthorized.guards';
 
 export const authRoutes: Routes = [
   {
     path: 'register',
     component: AuthFormComponent,
+    canActivate: [UserUnauthorizedAuthGuard],
   },
   {
     path: 'login',
     component: AuthFormComponent,
+    canActivate: [UserUnauthorizedAuthGuard],
   },
 ];
