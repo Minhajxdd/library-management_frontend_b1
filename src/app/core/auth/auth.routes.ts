7e8d7d731c787edf0069c00a6a1add33194e5ec5
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-08 09:54:30 +0530

    feat(auth): registerd route

--- src/app/core/auth/auth.routes.ts
+++ src/app/core/auth/auth.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { AuthFormComponent } from './pages/auth-form/auth-form.component';
 import { UserUnauthorizedAuthGuard } from './guards/user-unauthorized.guards';
+import { AuthAdminPageComponent } from './pages/auth-admin-page/auth-admin-page.component';
 
 export const authRoutes: Routes = [
   {
@@ -11,6 +12,11 @@ export const authRoutes: Routes = [
   {
     path: 'login',
     component: AuthFormComponent,
+    canActivate: [UserUnauthorizedAuthGuard],
+  },
+  {
+    path: 'admin/login',
+    component: AuthAdminPageComponent,
     canActivate: [UserUnauthorizedAuthGuard],
   },
 ];
