8ccec7a32d2f3f22e8a536b29741d702ef09b5b5
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-08 17:38:16 +0530

    feat(admin): admin navbar added

--- src/app/features/admin/pages/admin-dashboard/admin-dashboard.component.ts
+++ src/app/features/admin/pages/admin-dashboard/admin-dashboard.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { AdminNavbarComponent } from "../../../../shared/components/navbars/admin-navbar/admin-navbar.component";
 
 @Component({
   selector: 'app-admin-dashboard',
-  imports: [],
+  imports: [AdminNavbarComponent],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css',
 })
