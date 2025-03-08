b1c3512634f009d5191b9653ad8a8a14bc0b94be
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-08 20:33:54 +0530

    feat(admin): router register on navbar

--- src/app/shared/components/navbars/admin-navbar/admin-navbar.component.ts
+++ src/app/shared/components/navbars/admin-navbar/admin-navbar.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-admin-navbar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './admin-navbar.component.html',
   styleUrl: './admin-navbar.component.css'
 })
