270d282753c3b74aaa47e8788d8a02035f1cbb82
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-08 09:36:43 +0530

    feat(dashboard): added search design

--- src/app/features/books/book-dashboard/book-dashboard.component.ts
+++ src/app/features/books/book-dashboard/book-dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { UserNavbarComponent } from '../../../shared/components/navbars/user-navbar/user-navbar.component';
+import { DahsboardSearchComponent } from "./dahsboard-search/dahsboard-search.component";
 
 @Component({
   selector: 'app-book-dashboard',
-  imports: [UserNavbarComponent],
+  imports: [UserNavbarComponent, DahsboardSearchComponent],
   templateUrl: './book-dashboard.component.html',
   styleUrl: './book-dashboard.component.css'
 })
