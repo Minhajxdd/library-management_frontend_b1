fda71f73850879467796779836e9e9f1bfd5a109
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-09 17:10:25 +0530

    feat(admin-navbar); added logout

--- src/app/shared/components/navbars/admin-navbar/admin-navbar.component.ts
+++ src/app/shared/components/navbars/admin-navbar/admin-navbar.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../../../core/auth/service/auth.service';
 
 @Component({
   selector: 'app-admin-navbar',
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './admin-navbar.component.html',
-  styleUrl: './admin-navbar.component.css'
+  styleUrl: './admin-navbar.component.css',
 })
 export class AdminNavbarComponent {
+  private readonly authService = inject(AuthService);
 
+  onLogout() {
+    this.authService.logout();
+  }
 }
