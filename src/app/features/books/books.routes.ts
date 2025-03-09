69c583ad07bfc641d7a18397ce4b3157696e9938
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-09 21:25:05 +0530

    feat(borrowed-books); component aded as well as route

--- src/app/features/books/books.routes.ts
+++ src/app/features/books/books.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { BookDashboardComponent } from './pages/book-dashboard/book-dashboard.component';
 import { UserAuthorizedGuard } from '../../core/auth/guards/user.authorized.guards';
+import { BorrowedBooksComponent } from './pages/borrowed-books/borrowed-books.component';
 
 export const booksRoute: Routes = [
   {
@@ -11,6 +12,11 @@ export const booksRoute: Routes = [
   {
     path: 'dashboard',
     component: BookDashboardComponent,
+    canActivate: [UserAuthorizedGuard],
+  },
+  {
+    path: 'borrowed-books',
+    component: BorrowedBooksComponent,
     canActivate: [UserAuthorizedGuard],
   },
   { path: '**', redirectTo: 'dashboard' },
