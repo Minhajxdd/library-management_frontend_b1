7a436a81f21cf093e2a50d2bff6bf097f2cac156
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-09 20:40:45 +0530

    feat(book): borrow book to backend done

--- src/app/features/books/book-dashboard/book-due-form/book-due-form.component.ts
+++ src/app/features/books/book-dashboard/book-due-form/book-due-form.component.ts
@@ -1,4 +1,11 @@
-import { Component, ElementRef, inject, input, output } from '@angular/core';
+import {
+  Component,
+  DestroyRef,
+  ElementRef,
+  inject,
+  input,
+  output,
+} from '@angular/core';
 import {
   AbstractControl,
   FormBuilder,
@@ -6,6 +13,7 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { BookDueFormService } from './book-due-form.service';
 
 @Component({
   selector: 'app-book-due-form',
@@ -18,15 +26,17 @@ export class BookDueFormComponent {
   onClose = output<boolean>();
 
   private readonly ele = inject(ElementRef);
+  private readonly bookDueFormService = inject(BookDueFormService);
+  private readonly destoryRef = inject(DestroyRef);
 
   dateForm: FormGroup;
   minDate: string;
-  
+
   constructor(private fb: FormBuilder) {
     this.minDate = new Date().toISOString().split('T')[0];
-  
+
     this.dateForm = this.fb.group({
-      date: ['', [Validators.required, this.dateValidator]]
+      date: ['', [Validators.required, this.dateValidator]],
     });
   }
 
@@ -35,30 +45,48 @@ export class BookDueFormComponent {
       return null;
     }
     const selectedDate = new Date(control.value);
-    
+
     const today = new Date();
     today.setHours(0, 0, 0, 0);
 
     if (selectedDate < today) {
       return { pastDate: 'Past dates are not allowed.' };
     }
-    
+
     const thirtyDaysLater = new Date(today);
     thirtyDaysLater.setDate(today.getDate() + 30);
-    
+
     if (selectedDate > thirtyDaysLater) {
-      return { dateOutOfRange: 'Please select a date within 30 days from today.' };
+      return {
+        dateOutOfRange: 'Please select a date within 30 days from today.',
+      };
     }
     return null;
   }
 
   onSubmit() {
     if (this.dateForm.valid) {
-      console.log('Form Data:', this.dateForm.value);
+      const date: string = this.dateForm.value.date;
+
+      this.borrowBook(date);
     } else {
       console.log('Form is invalid:', this.dateForm.errors);
       this.dateForm.markAllAsTouched();
     }
+  }
+
+  borrowBook(date: string) {
+    const subscription = this.bookDueFormService
+      .borrowBook(this.bookId(), date)
+      .subscribe({
+        next: (data) => {
+          console.log(data);
+        },
+      });
+
+    this.destoryRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
   }
 
   onToggleClose() {
---
+++ src/app/features/books/book-dashboard/book-due-form/book-due-form.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BookDueFormService {
+  private readonly http = inject(HttpClient);
+
+  borrowBook(bookId: string, dueDate: string) {
+    const query = { bookId, dueDate };
+    return this.http.post(
+      `${environment.back_end}/transactions/borrow`,
+      query,
+      { withCredentials: true }
+    );
+  }
+}
