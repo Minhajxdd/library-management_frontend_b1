438e0bce4169817870a728477a0f44e3b615083d
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-09 18:17:21 +0530

    feat(due): due date validation added

--- src/app/features/books/book-dashboard/book-due-form/book-due-form.component.ts
+++ src/app/features/books/book-dashboard/book-due-form/book-due-form.component.ts
@@ -1,20 +1,66 @@
 import { Component, ElementRef, inject, input, output } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-book-due-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './book-due-form.component.html',
-  styleUrl: './book-due-form.component.css'
+  styleUrl: './book-due-form.component.css',
 })
 export class BookDueFormComponent {
-  bookId = input.required<string>()
+  bookId = input.required<string>();
   onClose = output<boolean>();
 
   private readonly ele = inject(ElementRef);
 
-  onToggleClose() {
-    this.onClose.emit(false);
-    this.ele.nativeElement.remove()
+  dateForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.dateForm = this.fb.group({
+      date: ['', [Validators.required, this.dateWithin30DaysValidator]],
+    });
   }
 
+  dateWithin30DaysValidator(
+    control: AbstractControl
+  ): { [key: string]: any } | null {
+    if (!control.value) {
+      return null;
+    }
+    const selectedDate = new Date(control.value);
+
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    const thirtyDaysLater = new Date(today);
+    thirtyDaysLater.setDate(today.getDate() + 30);
+
+    if (selectedDate > thirtyDaysLater) {
+      return {
+        dateOutOfRange: 'Please select a date within 30 days from today',
+      };
+    }
+    return null;
+  }
+
+  onSubmit() {
+    console.log(`Called On Submit`);
+    if (this.dateForm.valid) {
+      console.log('Form Data:', this.dateForm.value);
+    } else {
+      console.log('Form is invalid:');
+      this.dateForm.markAllAsTouched();
+    }
+  }
+
+  onToggleClose() {
+    this.onClose.emit(false);
+    this.ele.nativeElement.remove();
+  }
 }
